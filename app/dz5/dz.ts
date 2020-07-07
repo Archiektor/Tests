1927930cf98b9e9b7b61b60353e65a725e81cf28
Nikki <archiektor@gmail.com>
2020-07-07 09:57:55 +0200

    - refactor tests

--- app/dz5/dz.ts
+++ app/dz5/dz.ts
@@ -7,7 +7,7 @@
 // 2. максимальное кол-во симоволов в слове.
 // Функция должна вернуть массив из слов, которые не привышают заданную длинну либо [].??
 
-export const returnArrayWithCondition = (strLine: string, maxChar: number) => {
+export const returnArrayWithCondition = (strLine: string, maxChar: number) : Array<string> => {
     if (!isFinite(maxChar) || maxChar === null || maxChar < 0) throw new Error();
     if (strLine.length === undefined || strLine === null) throw new Error();
     let newStrLine = strLine.split(" ").map(word => word.trim().replace(" ", '').replace("-", ''));
@@ -27,7 +27,7 @@ export const returnArrayWithCondition = (strLine: string, maxChar: number) => {
 //      [ '1',2,'3',4,'5','6',7,'8',9,10 ] => [ 10, 9, 8 ]
 //      [ 10, '10', '10', 10 ] => [ 10, 10, 10 ]
 
-export const returnBiggest = (arr: Array<any>) => {
+export const returnBiggest = (arr: Array<any>) : Array<number> | [] => {
     for (const value of arr) {
         if (!(parseInt(value) > 0 && parseInt(value) < 100)) throw new Error();
     }
@@ -46,10 +46,10 @@ export const returnBiggest = (arr: Array<any>) => {
 //      [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
 //      [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
 
-export const modifyArray = (arr: Array<number>) => {
+export const modifyArray = (arr: Array<number>): Array<number> => {
     let newArr = [];
     for (let i = 0; i < arr.length; i++) {
-        if(!isFinite(arr[i]) || arr[i] === null) throw new Error();
+        if (!isFinite(arr[i]) || arr[i] === null) throw new Error();
         newArr.push(arr[i] + arr.slice(0, i).reduce((acc, number) => acc + number, 0))
     }
     return newArr;
@@ -59,3 +59,11 @@ export const modifyArray = (arr: Array<number>) => {
 // возвращает одномерный(плоский, одного уровня) массив элементов.
 // [1,[1,2,[3,[4,5,[6],10]],8],9] => [1,1,2,3,4,5,6,10,8,9]
 // Вывести его в консоль
+
+// export const flatArray = (arr: Array<number>) : Array<number> => arr.flat(Infinity);
+
+export const flatten = (arr: Array<number>): Array<number> => {
+    return arr.reduce(function (flat, toFlatten) {
+        return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+    }, []);
+}
